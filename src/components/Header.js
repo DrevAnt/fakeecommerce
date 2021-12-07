ba4388d60d37e8dd8fd8648bd349b3cfa940e5ed
Anton Drevetskyi <drevetskyianton@gmail.com>
2021-12-06 21:32:59 -0700

    "better routing"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  const history = useHistory();
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2>ShopName</h2>
+        <h2 onClick={() => history.push(`/`)}>ShopName</h2>
       </div>
     </div>
   );
